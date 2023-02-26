b6ac1162d5b981add12fedefa002985b95de460e
Jaeseok Park <jaesukpark77@gmail.com>
2023-02-26 22:26:47 +0900

    Add. 6.1 useEffect - Dependencies(종속성 배열) 학습

--- 2. ReactJS로 영화 웹 서비스 만들기/5. Creat React App/cra/src/App.js
+++ 2. ReactJS로 영화 웹 서비스 만들기/5. Creat React App/cra/src/App.js
@@ -2,13 +2,27 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyWord] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('i run all the time');
+  const onChange = (e) => setKeyWord(e.target.value);
   useEffect(() => {
-    console.log('call the api');
+    console.log('I run only once');
   }, []);
+  useEffect(() => {
+    /*     if (keyword !== "" && keyword.length > 5) {
+          console.log('SEARCH FOR', keyword);
+        } */
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword & counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
