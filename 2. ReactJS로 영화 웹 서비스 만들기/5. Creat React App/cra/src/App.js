6935c85dd0988c08d9f4c60e11dd148141e97ef6
Jaeseok Park <jaesukpark77@gmail.com>
2023-02-28 09:34:07 +0900

    Add. 7.2 Coin Tracker 학습 - 추가적인 실습

--- 2. ReactJS로 영화 웹 서비스 만들기/5. Creat React App/cra/src/App.js
+++ 2. ReactJS로 영화 웹 서비스 만들기/5. Creat React App/cra/src/App.js
@@ -3,30 +3,48 @@ import { useState, useEffect } from 'react';
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [cost, setCost] = useState(1);
+  const [need, setNeed] = useState(0);
+
+  const onChange = (e) => {
+    setCost(e.target.value);
+    setNeed(1);
+  }
+
+  const handleInput = (e) => {
+    setNeed(e.target.value)
+    console.log(need);
+  }
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
         setCoins(json);
         setLoading(false);
-        console.log(json);
       });
   }, [])
   return (
-    <div>
-      <h1>The Coins!({loading ? "" : `${coins.length}`})</h1>
+    <>
+      <h1>The Coins!({loading ? "" : `Here are ..${coins.length} coins`})</h1>
       {loading ?
         <strong>Loading...</strong>
         :
-        <select>
+        <select onChange={onChange}>
+          <option>Select Coin!</option>
           {coins.map((coin) => 
-            <option>
+            <option key={coin.id} value={coin.quotes.USD.price}>
               {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
             </option>
           )}
         </select>
       }
-    </div>
+      <h2>Please enter the amount</h2>
+      <div>
+        <input value={need} onChange={handleInput} type="number" placeholder='input amount of money' />$
+      </div>
+      <h2>You can get {need / cost}</h2>
+    </>
   );
 }
 
