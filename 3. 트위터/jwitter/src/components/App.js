6060666c2297af81947df35b9acc72d30fe8026f
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-04 22:01:20 +0900

    Add. 2.3 Twitter Cloning 학습 - Log In

--- 3. 트위터/jwitter/src/components/App.js
+++ 3. 트위터/jwitter/src/components/App.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppRouter from 'components/Router';
 import {auth} from 'fbase';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(auth.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true)
+    });
+  }, [])
+  // setInterval(() => {
+  //   console.log(auth.currentUser);
+  // }, 2000);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : 'Initializing...'}
       <footer>&copy; { new Date().getFullYear() } Jwitter</footer>
     </>
   )
--- 3. 트위터/jwitter/src/routes/Auth.js
+++ 3. 트위터/jwitter/src/routes/Auth.js
@@ -9,6 +9,7 @@ const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [newAcount, setNewAccount] = useState(true);
+  const [error, setError] = useState('');
   const onChange = (e) => {
     const { target: { name, value } } = e;
     if (name === 'email') {
@@ -31,10 +32,13 @@ const Auth = () => {
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      setError(error.message);
     }
   };
 
+  const toggleAccount = () => setNewAccount(prev => !prev);
+    
+    
   return (
     <>
       <form onSubmit={onSubmit}>
@@ -55,7 +59,11 @@ const Auth = () => {
           onChange={onChange}
         />
         <input type="submit" value={newAcount ? "Create Account" : "Log In"} />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAcount ? 'Sign In' : 'Create Account'}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
