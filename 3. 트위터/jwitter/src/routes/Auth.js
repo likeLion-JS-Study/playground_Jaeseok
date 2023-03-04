4e87a6d4d5d117fbb0aba7de3c2f8b8dfd44e225
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-04 20:34:49 +0900

    Add. 2.3 Twitter Cloning 학습 - Creating Account

--- 3. 트위터/jwitter/src/routes/Auth.js
+++ 3. 트위터/jwitter/src/routes/Auth.js
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+import {
+createUserWithEmailAndPassword,
+getAuth,
+signInWithEmailAndPassword,
+} from 'firebase/auth';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAcount, setNewAccount] = useState(true);
   const onChange = (e) => {
     const { target: { name, value } } = e;
     if (name === 'email') {
@@ -11,11 +17,26 @@ const Auth = () => {
       setPassword(value);
     }
   }
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
-  }
+    try {
+      let data;
+      const auth = getAuth();
+      if (newAcount) {
+        // create 
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        // log in
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <div>
+    <>
       <form onSubmit={onSubmit}>
         <input
           name='email'
@@ -33,13 +54,13 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAcount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
       </div>
-    </div>
+    </>
   )
 }
 
