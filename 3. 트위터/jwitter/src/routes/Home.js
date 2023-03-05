9686ca7eab1de677d3b5a399d6c23a7fb548ced9
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-05 23:26:30 +0900

    Add. 4.1 Twitter Cloning 학습 - Preview Images

--- 3. 트위터/jwitter/src/routes/Home.js
+++ 3. 트위터/jwitter/src/routes/Home.js
@@ -6,6 +6,7 @@ import Jweet from 'components/Jweet';
 const Home = ({ userObj }) => {
   const [jweet, setJweet] = useState('');
   const [jweets, setJweets] = useState([]);
+  const [attachment, setAttachment] = useState(null);
   useEffect(() => {
     const q = query(collection(dbService, "jweets"));
     onSnapshot(q, (snapshot) => {
@@ -32,12 +33,35 @@ const Home = ({ userObj }) => {
   const onChange = (e) => {
     const { target: { value } } = e;
     setJweet(value);
+  };
+  const onFileChange = (e) => {
+    const { target: { files } } = e;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const { currentTarget: {result} } = finishedEvent;
+      setAttachment(result);
+    }
+    reader.readAsDataURL(theFile);
   }
+  const onClearAttachment = () => setAttachment(null)
   return (
     <>
       <form onSubmit={onSubmit}>
-        <input value={jweet} onChange={onChange} type="text" placeholder="What's on your mind" maxLength={120} />
+        <input
+          value={jweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind"
+          maxLength={120} />
+        <input type="file" accept='image/*' onChange={onFileChange} />
         <input type="submit" value="Jweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} alt="ex" width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {jweets.map((jweet) => (
