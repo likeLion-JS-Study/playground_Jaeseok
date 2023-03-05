59e3879d027c7bae6aa071936c030285cdc3e836
Jaeseok Park <jaesukpark77@gmail.com>
2023-03-05 10:59:04 +0900

    Add. 3.1 Twitter Cloning 학습 - Form and Database Setup

--- 3. 트위터/jwitter/src/routes/Home.js
+++ 3. 트위터/jwitter/src/routes/Home.js
@@ -1,5 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const [jweet, setJweet] = useState('');
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+  const onChange = (e) => {
+    const { target: { value } } = e;
+    setJweet(value);
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <input value={jweet} onChange={onChange} type="text" placeholder="What's on your mind" maxLength={120} />
+      <input type="submit" value="Jweet" />
+    </form>
+  )
+};
 
 export default Home;
